5d329360aaf494b5a43c6def56ffa21f04c06d79
bianquini <iagobianquini@gmail.com>
2020-08-19 23:14:06 -0300

    Created Methods to read, Add and Delete repos from backend.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(...repositories, response.data);
+    });
+  }, []);
+
+  function handleAddRepository() {
+    const newRepo = {
+      title: `Novo Repositório: ${Date.now()}`,
+      url: `http://localhost ${Date.now()}`,
+      techs: ["Java, spring"],
+    };
+
+    api.post("repositories", newRepo).then((response) => {
+      const repo = response.data;
+      console.log(repo);
+      setRepositories([...repositories, repo]);
+    });
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  function handleRemoveRepository(id) {
+    api.delete(`repositories/${id}`).then(() => {
+      setRepositories(repositories.filter((x) => x.id !== id));
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
